5909d00d3c377aa51f93e041dea188374e81296f
ValikKorovichenko <valik.korovichenko@ab-soft.net>
2023-02-19 14:11:04 +0200

    tes8_for_2 task completed

--- src/test/java/absoft/test8_for_2.java
+++ src/test/java/absoft/test8_for_2.java
@@ -5,6 +5,23 @@ package absoft;
 Если введенное пользователем число (n) меньше 1 или больше 20 - вывести текст "Ошибка, введите число от 1 до 20"
  */
 
-public class test8_for_2 {
+import java.util.Scanner;
 
+public class test8_for_2 {
+    public static void main(String[] args) {
+        int from = 1;
+        int to = 20;
+        System.out.print("Enter the number: ");
+        int n = new Scanner(System.in).nextInt();
+
+        int i = n;
+        if (i < from || i > to) {
+            System.out.println("Error, please enter a number between 1 and 20");
+        } else {
+            for (i = from; i <= n; i++) {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
+
