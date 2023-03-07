aba778ce6c387b5df23cec360a3b1d5d8686e89c
ValikKorovichenko <valik.korovichenko@ab-soft.net>
2023-03-07 10:53:41 +0200

    test12_methods2.java task completed

--- src/test/java/absoft/test12_methods2.java
+++ src/test/java/absoft/test12_methods2.java
@@ -9,6 +9,31 @@ package absoft;
 Если имя не "Alex", "John" или "Stan" - вывести в консоль "Sorry, name was not found"
  */
 
-public class test12_methods2 {
+import java.util.Scanner;
 
+public class test12_methods2 {
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+        String name = new Scanner(System.in).nextLine();
+        User(name);
+        Seat(name);
+    }
+
+    private static void User(String name) {
+        if (name.equals("Alex") || name.equals("John") || name.equals("Stan")) {
+            System.out.println("Hello " + name);
+        } else {
+            System.out.println("Sorry, name was not found");
+        }
+    }
+
+    private static void Seat(String name) {
+        if (name.equals("Alex")) {
+            System.out.println("Your seat is 1");
+        } else if (name.equals("John")) {
+            System.out.println("Your seat is 2");
+        } else if (name.equals("Stan")) {
+            System.out.println("Your seat is 3");
+        }
+    }
 }
