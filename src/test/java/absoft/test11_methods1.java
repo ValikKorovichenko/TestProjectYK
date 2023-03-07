9780dfb9b1a15fc32ac01dcba64446d5d8269e6d
ValikKorovichenko <valik.korovichenko@ab-soft.net>
2023-03-07 10:33:39 +0200

    test11_methods1 task completed

--- src/test/java/absoft/test11_methods1.java
+++ src/test/java/absoft/test11_methods1.java
@@ -7,6 +7,20 @@ Price может быть как целым числом так и дробны�
 Вывести в консоль "Your total = " + результат
  */
 
-public class test11_methods1 {
+import java.util.Scanner;
 
+public class test11_methods1 {
+    public static void main(String[] args) {
+        System.out.print("Enter the Price: ");
+        double price = new Scanner(System.in).nextDouble();
+        System.out.print("Enter the Quantity: ");
+        int qty = new Scanner(System.in).nextInt();
+        double total = total(price, qty);
+        System.out.println("Your total = " + total);
+    }
+
+    private static double total(double price, int qty) {
+        double total = price * qty * 0.8;
+        return total;
+    }
 }
